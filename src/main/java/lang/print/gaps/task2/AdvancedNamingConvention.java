b0245e2b3323a5c0231e8cc54b3aef00cdea95a9
amirkaidarov <KaidarovAmir1441@gmail.com>
2022-07-26 20:24:26 +0600

    a

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULTAGE = 18;
 
     private int age;
     private int phoneNumber;
 
-    public void callToFriend() {
+    public static void callToFriend() {
         //some code
     }
 
-    public void callByNumber(int number) {
+    public static void callByNumber(int number) {
         //some code
     }
 }
