d6d89befd15d1564a50e7f9b4d0be4aa69a0dbec
amirkaidarov <KaidarovAmir1441@gmail.com>
2022-07-26 20:39:09 +0600

    a

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,6 +6,7 @@ public class NamingConvention {
         int variable_one = 1;
         int variable_two = 2;
         int variable_three = 3;
+
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
         int variable_six = 6;
